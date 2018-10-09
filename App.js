f6c006bbc09e4566d0cbbae40e9904147f514170
Emmanuel Moran <emmanuel.moran@uabc.edu.mx>
2018-10-09 11:50:39 -0700

    firebase error fixed and loginForm works well

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Text, View } from 'react-native';
+import ReduxThunk from 'redux-thunk';
 import reducers from './src/reducers';
-import firebase from '@firebase/app';
+import firebase from 'firebase';
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component{
@@ -21,8 +22,10 @@ class App extends Component{
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
